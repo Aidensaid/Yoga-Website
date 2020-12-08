fd9e65bd3f77d17e9147d8070fa66c14e8632ca3
Aiden Loughlin <aiden@aidens-air.home>
2020-12-08 11:41:30 +1100

    Delete files

--- yoga-site/yoga-website/pages/index.js
+++ yoga-site/yoga-website/pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas enim id quam aliquam accumsan. 
             Nullam a justo risus. Nam sapien leo, scelerisque at lobortis ut, ornare et nisl. Morbi sagittis auctor neque, at elementum massa aliquam in. 
             Etiam quis pharetra neque. Maecenas lectus purus, pretium in sodales sed, varius eget ante. 
-            Ut aliquet, tortor sit amet efficitur sagittis, elit risus posuere mi, sed placerat est libero in augue.
+            Ut aliquet, tortr sit amet efficitur sagittis, elit risus posuere mi, sed placerat est libero in augue.
           </p>
           <Divider />
         </Container>
